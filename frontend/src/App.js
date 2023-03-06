851acf519e6a9488cd257aebbfd41ae8806c853d
Prem-rao <raopremwaghmare@gmail.com>
2023-03-06 16:21:46 +0530

    created form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,46 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-// import Home from "./components/Home";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Home from "./components/Home";
 // import Blog from "./components/Blog";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
-	 <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Ticket Generating System</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/Home">Ticket Generating System</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/Home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Home" element={<Home/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,30 @@
 import React from "react";
-import "../StyleSheet/home.css"
-const Home=()=>{
+import "../StyleSheet/home.css";
+const Home = () => {
   return (
     <>
-    <h1>Home</h1>
+      <h1>Raise Issue</h1>
+      <form>
+        <label>
+          USER ID: <input type="text" name="userid" />
+        </label>
+        <br />
+        <br />
+        <label>
+          Issue: <input type="text" name="issue" id="" />
+        </label>
+        <br />
+        <br />
+        <label>
+          Department: <input type="text" name="dept" id="" />
+        </label>
+        <br />
+        <br />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
     </>
-  )
-}
+  );
+};
 export default Home;
