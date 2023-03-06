e93fe759169e9cbdea378e3b479e8db5b1186f13
Prem-rao <raopremwaghmare@gmail.com>
2023-03-06 15:39:44 +0530

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,40 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-function App(){
-	
-
-	return(
-		<>
-			<Navbar/>
-			<h1>Hello</h1>
-		</>
-	);
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Home from "./components/Home";
+import Blog from "./components/Blog";
+import "./App.css";
+import test from "./App.test";
+function App() {
+  return (
+    <>
+	 <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+  );
 }
 export default App;
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../StyleSheet/home.css"
+const Home=()=>{
+  return (
+    <>
+    <h1>Home</h1>
+    </>
+  )
+}
+export default Home;
